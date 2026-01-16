a4501fbfb48d88f0f5ec025c079a59e47881a320
ElfoAlienigena <amita.guzmancid@gmail.com>
2026-01-15 21:56:45 -0300

    arreglo errores de conexion con db

--- Visual-Empresa/crud/server/config/db.js
+++ Visual-Empresa/crud/server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'starmaN1', // Recuerda poner tu contraseña real
+    password: '', // Recuerda poner tu contraseña real
     database: 'intranet_inventario' 
 });
 
